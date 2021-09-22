141f6d2423a5449cc92b2f91c4afb2a101dd5ae2
Felix <felixrunye@gmail.com>
2021-09-22 10:32:05 +0300

    updated selecting files

--- components/mainFeed/InputBox.js
+++ components/mainFeed/InputBox.js
@@ -2,33 +2,75 @@ import { useSession } from "next-auth/client"
 import Image from 'next/image';
 import { EmojiHappyIcon,  } from '@heroicons/react/outline';
 import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid';
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { db } from '../../firebase';
+import firebase from "firebase";
 
 function InputBox() {
     const [session] = useSession();
 
     const [input,setInputState] =  useState('');
     const inputRef = useRef(null);
-    
-    const sendPost = e =>{
+    const filePickerRef= useRef(null);
+    const [imageToPost, setImageToPost] = useState('');
+
+    const  sendPost =  e =>{
         e.preventDefault();
-        
-        console.log("sent message");
+        console.log("send post clicked");
+        if(!inputRef.current.value) {
+            console.log("no value")
+        } //if empty return empty
+
+        db.collection('posts').add({
+            message: inputRef.current.value,
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+            timestamp : firebase.firestore.FieldValue.serverTimestamp()
+
+        }).then((res)=>{
+            console.log("saved");
+            console.log(res);
+        }).catch((error)=>{
+            console.log(error)
+        })
+        //clear the input
+        inputRef.current.value ='';
+    }
+   const addImageToPost =(e)=>{
+       console.log("file selected")
+        const reader = new FileReader();
+        if(e.target.files[0]){
+            reader.readAsDataURL(e.target.files[0])
+        }
+        reader.onload = (readerEvent)=>{
+            setImageToPost(readerEvent.target.result);
+        }
+    }
+
+    const removeImage =()=>{
+        setImageToPost(null);
     }
     return (
         <div className="bg-white p-2 mt-6 rounded-2xl shadow-md font-medium">
            <div className="flex space-x-4 p-4 items-center">
-           {
-               session && (
-                <Image src={session.user.image} className="rounded-full" width={40} height={40} layout="fixed"/>
-               )
-           }
-           <form className="flex flex-1">
-               <input type="text" placeholder={`whats on your mind ${session ? session.user.name : ''} ?`} 
-               className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"/>
-               <button type="submit" className="hidden" onClick={sendPost}></button>
-           </form>
-
+            {
+                session && (
+                    <Image src={session.user.image} className="rounded-full" width={40} height={40} layout="fixed"/>
+                )
+            }
+            <form className="flex flex-1">
+                <input type="text" placeholder={`whats on your mind ${session ? session.user.name : ''} ?`} 
+                className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none" ref={inputRef}/>
+                <button type="submit" className="hidden" onClick={sendPost}></button>
+            </form>
+            
+            { imageToPost && (
+                <div className="flex flex-col filter hover:brightness-105 transition duration-150 transform hover:scale-x-105 cursor-pointer" onClick={removeImage} >
+                    <img className="h-10 object-contain" src={imageToPost} alt="" />
+                    <p className="text-xs text-red-500 text-center">Remove</p>
+                </div>
+            )}
             
            </div>
 
@@ -39,11 +81,12 @@ function InputBox() {
                         Live Video
                     </p>
                </div>
-               <div className="inputIcon">
+               <div className="inputIcon" onClick={()=>filePickerRef.current.click()}>
                     <CameraIcon className="h-7 text-green-500 " />
                     <p className="text-xs md:text-sm xl:text-base">
                         Photo / Video
                     </p>
+                    <input type="file" hidden onChange={addImageToPost} ref={filePickerRef}/>
                </div>
                <div className="inputIcon">
                     <EmojiHappyIcon className="h-7 text-yellow-300 " />
--- firebase.js
+++ firebase.js
@@ -1,19 +1,29 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 import "firebase/storage";
 
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID
+//   };
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-  };
+  apiKey: "AIzaSyAMJ3Ct7Hb0R3qnz4wGPbB4xnR2E5ScBQY",
+  authDomain: "celtic-volt-494.firebaseapp.com",
+  databaseURL: "https://celtic-volt-494.firebaseio.com",
+  projectId: "celtic-volt-494",
+  storageBucket: "celtic-volt-494.appspot.com",
+  messagingSenderId: "939249142404",
+  appId: "1:939249142404:web:6ed73fca72a550a77210d1"
+};
+
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
-const db= firebase.firestore();
+const db= app.firestore();
 const storage = firebase.storage();
 
 export { db, storage};
